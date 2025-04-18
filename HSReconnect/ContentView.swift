aa65e4a8cb57e1c293ab69b8c6c52a831583bf82
hezt <me@hezt.net>
2025-04-18 19:17:40 -0400

    Initial Commit

---
+++ HSReconnect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HSReconnect
+//
+//  Created by Zitong on 4/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HSReconnect/HSReconnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSReconnectApp.swift
+//  HSReconnect
+//
+//  Created by Zitong on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct HSReconnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
